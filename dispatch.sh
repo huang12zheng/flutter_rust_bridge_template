d996e4baea98ff24199d47c1c124adefe1fbb0b7
huang12zheng <805104533@qq.com>
2022-11-25 22:14:05 +0800

    feat: use run

--- dispatch.sh
+++ dispatch.sh
@@ -1,4 +1,12 @@
-# cp -R . .. 
+repo="https://github.com/huang12zheng/flutter_rust_bridge_template"
+
 git clone https://github.com/Desdaemon/flutter_rust_bridge_template app_schema
-ls . |grep -v "app_schema" | awk '{ print "cp -R "$0" app_schema"}'
+# cp -R . .. 
+ls . |grep -v "app_schema" | awk '{ print "cp -R "$0" app_schema"}' | sh
 cd app_schema && rm dispatch.sh
+
+git remote rm origin
+git remote add origin $repo
+git push --set-upstream origin patch
+git add *
+git commit -m "patch finish"
