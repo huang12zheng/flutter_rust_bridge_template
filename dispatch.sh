463fefff90f504f33000266ac3ae5f42fb33a4a6
huang12zheng <805104533@qq.com>
2022-11-25 22:09:39 +0800

    feature: use git clone template

--- dispatch.sh
+++ dispatch.sh
@@ -1,3 +1,4 @@
 # cp -R . .. 
-ls . | awk '{ print "cp -R "$0" .."}'
-cd .. && rm dispatch.sh
+git clone https://github.com/Desdaemon/flutter_rust_bridge_template app_schema
+ls . |grep -v "app_schema" | awk '{ print "cp -R "$0" app_schema"}'
+cd app_schema && rm dispatch.sh
